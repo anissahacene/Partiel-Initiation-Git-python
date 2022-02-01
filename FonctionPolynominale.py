479a611f32d3264e919ca6d1ff13a0f9f5b56e67
Anissa Hacene <94847120+anissahacene@users.noreply.github.com>
2022-02-01 16:37:03 +0100

    Add files via upload

---
+++ FonctionPolynominale.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Exercice 1: fonction polynominale
+
+# In[27]:
+
+
+def poly(x):
+    c = x**3-(1.5*(x**2))-(6*x)+5
+    return c
+
+
+# In[28]:
+
+
+poly(5)
+
+
+# In[17]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
